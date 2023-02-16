ffbc95b9f2537b3fd468e6c36c6d6119c8c2c57a
Brodie Kime <brodie.kime@gmail.com>
2023-02-16 12:05:25 -0500

    fixed half filled out bug

--- models/Fest.model.js
+++ models/Fest.model.js
@@ -11,7 +11,10 @@ const festSchema = new Schema({
     min: [0, "Rating must be between 0 and 100"], 
     max: [100, "Rating must be between 0 and 100"]
     },
-    imageUrl: String,
+    imageUrl: {
+    type: String,
+    default: "https://res.cloudinary.com/dc6w7a0c8/image/upload/v1676566878/movie-project/fest-pic_guwb8u.png"
+  },
     favSet: String,
     owner: { type: Schema.Types.ObjectId, ref: "User" },
     comments: [{type: Schema.Types.ObjectId, ref: "Comment"}]
