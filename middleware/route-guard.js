d834a0326659a4023e130f65885775568a209261
Brodie Kime <brodie.kime@gmail.com>
2023-02-17 10:09:22 -0500

    deployed only need to fix the nav bar words

--- middleware/route-guard.js
+++ middleware/route-guard.js
@@ -3,7 +3,6 @@
 const Fest = require('../models/Fest.model')
 const Comment = require('../models/Comment.model')
 
-// checks if the user is logged in when trying to access a specific page
 const isLoggedIn = (req, res, next) => {
     if (!req.session.user) {
       return res.redirect('/users/login');
@@ -11,13 +10,11 @@ const isLoggedIn = (req, res, next) => {
     next();
 };
   
-  // if an already logged in user tries to access the login page it
-  // redirects the user to the home page
 const isLoggedOut = (req, res, next) => {
-if (req.session.user) {
-    return res.redirect('/users/profile');
-}
-next();
+    if (req.session.user) {
+        return res.redirect('/users/profile');
+    }
+    next();
 };
 
 const isOwner = (req, res, next) => {
@@ -51,22 +48,6 @@ const isCommentOwner = (req, res, next) => {
         console.log(err)
     })
 }
-
-// const isCommentOwner = (req, res, next) => {
-
-//     Fest.findById(req.params.id)
-//     .populate('owner')
-//     .then((foundFest) => {
-//         if (!req.session.user || foundFest.owner._id.toString() !== req.session.user._id) {
-//             res.redirect(`/fests/details/${req.params.id}`)
-//         } else {
-//             next()
-//         }
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     })
-// }
 
 // const isNotOwner = (req, res, next) => {
 
