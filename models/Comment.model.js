e319a93262dc255b4db0d43331ad363de5ce4311
Brodie Kime <brodie.kime@gmail.com>
2023-02-18 13:05:49 -0500

    fixed textarea on edits

--- models/Comment.model.js
+++ models/Comment.model.js
@@ -4,6 +4,6 @@ const commentSchema = new Schema({
     user: { type: Schema.Types.ObjectId, ref: "User" },
     comment: { type: String, maxlength: 200 },
     festId: {type: Schema.Types.ObjectId, ref: "Fest"}
-  });
+});
 
 module.exports = model('Comment', commentSchema);
